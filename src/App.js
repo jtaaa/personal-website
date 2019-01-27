9ab892f22d687f48835baffed1f0addcafdc2aa2
jtaaa <joshuatallum@gmail.com>
2019-01-27 05:10:49 -0500

    Add on page load animations

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './App.css';
 import LinkList from './LinkList/LinkList';
 
 class App extends Component {
+  constructor(props) {
+    super (props);
+
+    this.state = { fadedout: true };
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ fadedout: false }), 800);
+  }
   render() {
     return (
       <div className="App">
         <img 
-          className="App-backdrop" 
+          className={`App-backdrop ${this.state.fadedout ? 'faded-out' : ''}`}
           src="/assets/headshot.jpg"
           alt="Handsome headshot o.O" />
         <div className="App-content">
@@ -22,48 +31,62 @@ class App extends Component {
             linkGroups={[{
               groupName: 'personal',
               links: [{
+                name: 'github',
                 href: 'https://github.com/jtaaa/',
                 src: '/assets/octocat_small_color.png',
                 alt: 'GitHub profile',
                 tooltip: <span>View GitHub Profile</span>,
                 title: 'GitHub',
+                delay: 600,
               }, {
+                name: 'resume',
                 href: 'https://raw.githack.com/jtaaa/Resume/master/Resume-blue.pdf',
                 src: '/assets/resume_small_color_short.png',
                 alt: 'Résumé',
                 tooltip: <span>View Résumé</span>,
                 title: 'Résumé',
+                delay: 550,
               }, {
+                name: 'email',
                 href: 'mailto:joshuatallum@gmail.com',
                 src: '/assets/paperplane_small_color.png',
                 alt: 'Email address',
                 tooltip: <span>Contact Me</span>,
-                title: 'Email me'
+                title: 'Email me',
+                delay: 500,
               }],
             }, {
               groupName: 'affiliations',
               size: 'small',
               links: [{
+                name: 'teleios',
                 href: 'https://www.teleios-systems.com/',
                 src: '/assets/teleios_small_color_whitebg.png',
                 alt: 'Teleios Systems Limited',
                 tooltip: <span>Teleios Systems Limited</span>,
+                delay: 450,
               }, {
+                name: 'massy',
                 href: 'http://www.massygroup.com/home.aspx',
                 src: '/assets/massy_small_color.png',
                 alt: 'Massy Group',
                 tooltip: <span>Massy Technologies InfoCom</span>,
+                delay: 400,
               }, {
+                name: 'acs',
                 href: 'http://uwacs.club',
                 src: '/assets/acs_small_bw.png',
                 alt: 'University of Waterloo Association of Caribbean Students',
                 tooltip: <span>University of Waterloo<br/>Association of Caribbean Students</span>,
+                delay: 350,
               }, {
+                name: 'blueprint',
                 href: 'http://uwblueprint.org',
                 src: '/assets/bp_small_color.png',
                 alt: 'UW Blueprint',
                 tooltip: <span>Blueprint</span>,
                 round: true,
+                delay: 300,
               }],
              }]} />
         </div>
--- src/LinkList/LinkList.js
+++ src/LinkList/LinkList.js
@@ -1,55 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './LinkList.css';
 
 import ReactTooltip from 'react-tooltip';
 
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
-const LinkList = ({ linkGroups }) => {
-  const children = [];
-  for (let i = 0; i < linkGroups.length; i++) {
-    const { groupName, links, size } = linkGroups[i];
-    const group = (
-      <div key={groupName} className="LinkList-group">
-        { links.map(({ href, src, alt, tooltip, title, round = false }) => (
-        <div key={href} className="LinkList-link-container">
-          <a
-            className="LinkList-link"
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer">
-            <img 
-              data-tip
-              data-for={href}
-              className={`LinkList-link-img ${round ? 'LinkList-round-img' : ''}`}
-              style={{ width: size === 'small' ? '24px' : '32px' }}
-              src={src}
-              alt={alt} />
-          </a>
-          <ReactTooltip
-            disable={isMobile}
-            id={href}
-            type='light'
-            effect='solid'
-            place="bottom">
-            { tooltip }
-          </ReactTooltip>
-          { title &&
-          <div className="LinkList-link-title">{ title }</div> }
+class LinkList extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = { greyed: {} };
+  }
+  
+  componentDidMount() {
+    this.props.linkGroups.forEach(linkGroup => {
+      linkGroup.links.forEach(link => {
+        setTimeout(() => this.setState(state => ({ greyed: { [link.name]: true, ...state.greyed }})), link.delay ? link.delay : '500');
+      });
+    });
+  }
+  
+  
+  render() {
+    const { linkGroups } = this.props;
+    const children = [];
+    for (let i = 0; i < linkGroups.length; i++) {
+      const { groupName, links, size } = linkGroups[i];
+      const group = (
+        <div key={groupName} className="LinkList-group">
+          { links.map(({ name, href, src, alt, tooltip, title, round = false }) => (
+          <div key={name} className="LinkList-link-container">
+            <a
+              className="LinkList-link"
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer">
+              <img 
+                data-tip
+                data-for={name}
+                className={`
+                  LinkList-link-img
+                  ${round ? 'LinkList-round-img' : ''}
+                  ${!this.state.greyed[name] ? 'LinkList-color-img' : ''}
+                `}
+                style={{ width: size === 'small' ? '24px' : '32px' }}
+                src={src}
+                alt={alt} />
+            </a>
+            <ReactTooltip
+              disable={isMobile}
+              id={name}
+              type='light'
+              effect='solid'
+              place="bottom">
+              { tooltip }
+            </ReactTooltip>
+            { title &&
+            <div className="LinkList-link-title">{ title }</div> }
+          </div>
+          )) }
         </div>
-        )) }
+      );
+      children.push(group);
+      if (i !== linkGroups.length - 1) {
+        children.push(<div key={`LinkList-divider-${i}`} className="LinkList-divider"></div>);
+      }
+    }
+    return (
+      <div className="LinkList">
+        { children }
       </div>
     );
-    children.push(group);
-    if (i !== linkGroups.length - 1) {
-      children.push(<div className="LinkList-divider"></div>);
-    }
-  }
-  return (
-    <div className="LinkList">
-      { children }
-    </div>
-  );
+  };
 };
 
 export default LinkList
