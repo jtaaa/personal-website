baa0f5f59f51b894098bed5d70061852b82a2059
jtaaa <joshuatallum@gmail.com>
2019-01-27 01:58:54 -0500

    Mobile first redesign

--- src/App.js
+++ src/App.js
@@ -19,44 +19,53 @@ class App extends Component {
             On season 1 episode 18 of <a href="https://www.ctv.ca/This-Is-Us" target="_blank" rel="noopener noreferrer">This is Us</a>.<br/>
           </div>
           <LinkList 
-            links={[{
-              href: 'http://uwblueprint.org',
-              src: '/assets/bp_small_color.png',
-              alt: 'UW Blueprint',
-              tooltip: <span>Blueprint</span>,
-              round: true,
+            linkGroups={[{
+              groupName: 'personal',
+              links: [{
+                href: 'https://github.com/jtaaa/',
+                src: '/assets/octocat_small_color.png',
+                alt: 'GitHub profile',
+                tooltip: <span>View GitHub Profile</span>,
+                title: 'GitHub',
+              }, {
+                href: 'https://raw.githack.com/jtaaa/Resume/master/Resume-blue.pdf',
+                src: '/assets/resume_small_color_short.png',
+                alt: 'Résumé',
+                tooltip: <span>View Résumé</span>,
+                title: 'Résumé',
+              }, {
+                href: 'mailto:joshuatallum@gmail.com',
+                src: '/assets/paperplane_small_color.png',
+                alt: 'Email address',
+                tooltip: <span>Contact Me</span>,
+                title: 'Email me'
+              }],
             }, {
-              href: 'http://uwacs.club',
-              src: '/assets/acs_small_bw.png',
-              alt: 'University of Waterloo Association of Caribbean Students',
-              tooltip: <span>University of Waterloo<br/>Association of Caribbean Students</span>,
-            }, {
-              href: 'http://www.massygroup.com/home.aspx',
-              src: '/assets/massy_small_color.png',
-              alt: 'Massy Group',
-              tooltip: <span>Massy Technologies InfoCom</span>,
-            }, {
-              href: 'https://www.teleios-systems.com/',
-              src: '/assets/teleios_small_color_whitebg.png',
-              alt: 'Teleios Systems Limited',
-              tooltip: <span>Teleios Systems Limited</span>,
-            }, {
-              href: 'mailto:joshuatallum@gmail.com',
-              src: '/assets/paperplane_small_color.png',
-              alt: 'Email address',
-              tooltip: <span>Contact Me</span>,
-            }, {
-              href: 'https://raw.githack.com/jtaaa/Resume/master/Resume-blue.pdf',
-              src: '/assets/resume_small_color_short.png',
-              alt: 'Resume',
-              tooltip: <span>View Résumé</span>,
-            }, {
-              href: 'https://github.com/jtaaa/',
-              src: '/assets/octocat_small_color.png',
-              alt: 'GitHub profile',
-              tooltip: <span>View GitHub Profile</span>,
-            }]}
-            dividers={[ 'https://www.teleios-systems.com/' ]} />
+              groupName: 'affiliations',
+              size: 'small',
+              links: [{
+                href: 'https://www.teleios-systems.com/',
+                src: '/assets/teleios_small_color_whitebg.png',
+                alt: 'Teleios Systems Limited',
+                tooltip: <span>Teleios Systems Limited</span>,
+              }, {
+                href: 'http://www.massygroup.com/home.aspx',
+                src: '/assets/massy_small_color.png',
+                alt: 'Massy Group',
+                tooltip: <span>Massy Technologies InfoCom</span>,
+              }, {
+                href: 'http://uwacs.club',
+                src: '/assets/acs_small_bw.png',
+                alt: 'University of Waterloo Association of Caribbean Students',
+                tooltip: <span>University of Waterloo<br/>Association of Caribbean Students</span>,
+              }, {
+                href: 'http://uwblueprint.org',
+                src: '/assets/bp_small_color.png',
+                alt: 'UW Blueprint',
+                tooltip: <span>Blueprint</span>,
+                round: true,
+              }],
+             }]} />
         </div>
       </div>
     );
--- src/LinkList/LinkList.js
+++ src/LinkList/LinkList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './LinkList.css';
 
-import ReactTooltip from 'react-tooltip'
+import ReactTooltip from 'react-tooltip';
 
-const LinkList = ({ links, dividers = [] }) => {
-  return (
-    <div className="LinkList">
-      { links.map(({ href, src, alt, tooltip, round = false }) => (
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
+
+const LinkList = ({ linkGroups }) => {
+  const children = [];
+  for (let i = 0; i < linkGroups.length; i++) {
+    const { groupName, links, size } = linkGroups[i];
+    const group = (
+      <div key={groupName} className="LinkList-group">
+        { links.map(({ href, src, alt, tooltip, title, round = false }) => (
         <div key={href} className="LinkList-link-container">
           <a
             className="LinkList-link"
@@ -17,16 +22,32 @@ const LinkList = ({ links, dividers = [] }) => {
               data-tip
               data-for={href}
               className={`LinkList-link-img ${round ? 'LinkList-round-img' : ''}`}
+              style={{ width: size === 'small' ? '24px' : '32px' }}
               src={src}
               alt={alt} />
           </a>
-          <ReactTooltip id={href} type='light' effect='solid' place="bottom">
+          <ReactTooltip
+            disable={isMobile}
+            id={href}
+            type='light'
+            effect='solid'
+            place="bottom">
             { tooltip }
           </ReactTooltip>
-          { dividers.includes(href) &&
-          <div className="LinkList-divider"></div> }
+          { title &&
+          <div className="LinkList-link-title">{ title }</div> }
         </div>
-      )) }
+        )) }
+      </div>
+    );
+    children.push(group);
+    if (i !== linkGroups.length - 1) {
+      children.push(<div className="LinkList-divider"></div>);
+    }
+  }
+  return (
+    <div className="LinkList">
+      { children }
     </div>
   );
 };
