28667470b09eda408e0436374b50bcee141d0071
jtaaa <joshuatallum@gmail.com>
2019-01-27 21:20:31 -0500

    Move HomePage to its own folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-import HomePage from './pages/HomePage';
+import HomePage from './pages/HomePage/HomePage';
 
 class App extends Component {
   render() {
