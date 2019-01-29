1d1ae3530f34c86805596f55843ee1bd68a756d6
jtaaa <joshuatallum@gmail.com>
2019-01-29 18:55:57 -0500

    Add function event properties for MenuButton

--- src/components/MenuButton/MenuButton.js
+++ src/components/MenuButton/MenuButton.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import './MenuButton.css'
+import './MenuButton.css';
 
 class MenuButton extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = { open: false };
+  }
+
+  toggleMenu() {
+    if (this.props.onMenuToggle) {
+      this.props.onMenuToggle(!this.state.open);
+    } else if (this.state.open && this.props.onMenuClose) {
+      this.props.onMenuClose();
+    } else if (this.props.onMenuOpen) {
+      this.props.onMenuOpen();
+    }
+    this.setState(state => ({
+      open: !state.open,
+      ...state,
+    }));
+  }
+  
   render() {
     return (
-      <div className="MenuButton">
+      <div className="MenuButton" onClick={this.toggleMenu.bind(this)}>
       </div>
     )
   }
