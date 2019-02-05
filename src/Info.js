4e22ea5f6d4650fa45bd03f627c10be3332a9728
jtaaa <joshuatallum@gmail.com>
2019-02-05 16:29:21 -0500

    Update header info

--- src/Info.js
+++ src/Info.js
@@ -1,27 +1,36 @@
+const headerInfo = [{
+    logo: '/assets/bp_small_color.png',
+    title: 'Blueprint',
+    name: 'blueprint',
+    alt: 'Blueprint logo',
+    pos: 0,
+}, {
+    logo: '/assets/acs_small_bw.png',
+    title: 'Association of Caribbean Students',
+    name: 'acs',
+    alt: 'ACS logo',
+    pos: 1,
+}, {
+    logo: '/assets/teleios_small_color_whitebg.png',
+    title: 'Teleios Systems Limited',
+    name: 'teleios',
+    alt: 'Teleios logo',
+    pos: 2,
+}, {
+    logo: '/assets/massy_small_color.png',
+    title: 'Massy Technologies InfoCom',
+    name: 'massy',
+    alt: 'Massy Group logo',
+    pos: 3,
+}];
+
 const mapLocationToHeaderInfo = (location) => {
     switch (location) {
-        case '/info/blueprint': return {
-                logo: '/assets/bp_small_color.png',
-                title: 'Blueprint',
-                alt: 'Blueprint logo'
-            };
-        case '/info/acs': return {
-                logo: '/assets/acs_small_bw.png',
-                title: 'Association of Caribbean Students',
-                alt: 'ACS logo'
-            };
-        case '/info/teleios': return {
-                logo: '/assets/teleios_small_color_whitebg.png',
-                title: 'Teleios Systems Limited',
-                alt: 'Teleios logo'
-            };
-        case '/info/massy': return {
-                logo: '/assets/massy_small_color.png',
-                title: 'Massy Technologies InfoCom',
-                alt: 'Massy Group logo'
-            };
-        default:
-            return {};
+        case '/info/blueprint': return headerInfo[0];
+        case '/info/acs':       return headerInfo[1];
+        case '/info/teleios':   return headerInfo[2];
+        case '/info/massy':     return headerInfo[3];
+        default:                return {};
     }
 }
 
@@ -58,4 +67,4 @@ const getInfo = (location) => {
     return info[location];
 }
 
-export { mapLocationToHeaderInfo, getInfo };
+export { mapLocationToHeaderInfo, getInfo, headerInfo };
