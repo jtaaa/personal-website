a18802e58b6447378fefaa252163882e0225bd7d
jtaaa <joshuatallum@gmail.com>
2019-02-07 13:20:23 -0500

    Add sections

--- src/components/ProjectInfo/ProjectInfo.js
+++ src/components/ProjectInfo/ProjectInfo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ProjectInfo.css';
 
+import Section from './../Section/Section';
+
 const ProjectInfo = ({ img, alt, title, titleLink, description, sections }) => (
   <div className="ProjectInfo">
     <img className="ProjectInfo-img" src={img} alt={alt} />
@@ -8,6 +10,9 @@ const ProjectInfo = ({ img, alt, title, titleLink, description, sections }) => (
     ? <a className="ProjectInfo-title" href={titleLink} target="_blank" rel="noopener noreferrer">{ title }</a>
     : <div className="ProjectInfo-title">{ title }</div> }
     <div className="ProjectInfo-description">{ description }</div>
+    { sections.map(section => (
+      <Section key={section.name} {...section}></Section>
+    ))}
   </div>
 );
 
