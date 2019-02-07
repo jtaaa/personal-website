49d2e40a605c5643c10e6ecf928eec130cf0ebf7
jtaaa <joshuatallum@gmail.com>
2019-02-07 18:11:28 -0500

    Add breakdown subsection

--- src/components/Section/SubSection/SubSection.js
+++ src/components/Section/SubSection/SubSection.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './SubSection.css';
 
-const SubSection = ({ title, type, contents }) => {
+const SubSection = ({ title, type, contents, backgroundColor, color }) => {
   function renderContent() {
-    switch (type) {
-      case 'paragraph':
-        return contents.map((content, index) => (
-          <p key={index} className="SubSection-paragraph">{ content }</p>
-        ));
-      default:
-        return null;
+    if (contents) {
+      switch (type) {
+        case 'paragraph':
+          return contents.map((content, index) => (
+            <p key={index} className="SubSection-paragraph">{ content }</p>
+          ));
+        case 'breakdown':
+        case 'rundown':
+        case 'summary':
+        case 'breakdown of de rundown':
+        case 'summary of de breakdown of the rundown':
+            return (
+              <div className="SubSection-breakdown" style={{ backgroundColor, color }}>
+                { contents.map((content, index) => (
+                <div key={index} className="SubSection-breakdown-point">{ content }</div>
+                ))}
+              </div>
+            );
+        default:
+          return null;
+      }
     }
+    return null;
   }
 
   return (
