49e5328cdd040c9eec61534514bb0072a95e80c1
jtaaa <joshuatallum@gmail.com>
2019-02-08 02:18:00 -0500

    Add mongoose starter

---
+++ db/mongoose.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const CONFIG = require('./../config');
+
+console.log(CONFIG);
+
+mongoose.connect(`mongodb://${CONFIG.DB_USER}:${CONFIG.DB_PASS}@${CONFIG.DB_URI}`, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log(`Connected to ${CONFIG.DB_URI} as ${CONFIG.DB_USER}`);
+});
+
+module.exports = db;
