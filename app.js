ee11f19ee17ba34d9a9b1558a0e32928f4783ead
jtaaa <joshuatallum@gmail.com>
2019-02-08 02:04:56 -0500

    Add splash router, error handler and react frontend production server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const splashRouter = require('./routes/spash');
 
 const app = express();
 
@@ -13,6 +13,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/api/splash', splashRouter);
+
+// Production react frontend serving
+if (process.env.NODE_ENV === 'production') {
+  // Serve static React build folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Let React handle routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+// Error handler
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  if (err.statusCode) {
+    res.status(err.statusCode).json({ error: err });
+  } else {
+    res.status(500).json({ error: err });
+  }
+});
 
 module.exports = app;
