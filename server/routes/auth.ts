a4789ad7a6f9ff2e154506675964463afc137d5b
jtaaa <joshuatallum@gmail.com>
2019-02-10 20:19:05 -0500

    Redirect to home page

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -3,17 +3,13 @@ import { passport } from './../auth';
 
 const router = express.Router();
 
-router.get('/google',
-    passport.authenticate('google', { scope: ['profile'] }),
-    (req, res, next) => {
-  res.send(req);
-});
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 
 router.get('/google/callback', 
     passport.authenticate('google', { failureRedirect: '/pineapples' }),
     (req, res) => {
   console.log('authenticated');
-  res.redirect('/grapes');
+  res.redirect('/');
 });
 
 export default router;
