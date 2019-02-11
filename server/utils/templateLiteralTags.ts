beeb1a107f9089903f29ea14dea6bbd29824bd90
jtaaa <joshuatallum@gmail.com>
2019-02-10 19:55:15 -0500

    Add dope new mongoUrlScheme tag

--- server/utils/templateLiteralTags.ts
+++ server/utils/templateLiteralTags.ts
@@ -1,9 +1,11 @@
 const oneLine = (strs: TemplateStringsArray, ...values: Array<string>) =>
-  strs.reduce((acc, cur, index) =>
-    acc.concat(cur, values[index] || '')
-  )
-    .replace(/\s+/g, ' ')
+  strs.reduce((acc, cur, index) => acc.concat(cur, values[index] || ''))
+      .replace(/\s+/g, ' ');
+
+const mongoUrlScheme = (strs: TemplateStringsArray, ...values: Array<string>) =>
+  `mongodb://${values[0]}:${values[1]}@${values[2]}`;
 
 export {
   oneLine,
+  mongoUrlScheme,
 };
