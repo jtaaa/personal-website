5111e677b123408ce01ff8b80b629c5cc0d31b5f
jtaaa <joshuatallum@gmail.com>
2019-02-10 19:11:21 -0500

    Add GOOGLE_OAUTH keys to CONFIG

--- server/config.ts
+++ server/config.ts
@@ -2,4 +2,6 @@ export default {
   DB_URI: process.env.DB_URI,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
+  GOOGLE_OAUTH_CLIENT_ID: process.env.GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_CLIENT_SECRET: process.env.GOOGLE_OAUTH_CLIENT_SECRET,
 };
