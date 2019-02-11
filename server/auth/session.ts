579e371f3917290d999cc7d977115d37895c6482
jtaaa <joshuatallum@gmail.com>
2019-02-10 20:24:19 -0500

    Add session setup

---
+++ server/auth/session.ts
@@ -0,0 +1,28 @@
+import * as expressSession from 'express-session';
+import * as connectMongoDBSession from 'connect-mongodb-session';
+const MongoDBStore = connectMongoDBSession(expressSession);
+
+import CONFIG from '../config';
+import { mongoUrlScheme } from '../utils/templateLiteralTags';
+
+const sessionStore = new MongoDBStore({
+  uri: mongoUrlScheme`
+    user: ${CONFIG.DB_USER}
+    password: ${CONFIG.DB_PASS}
+    uri: ${CONFIG.DB_URI}
+  `,
+  collection: 'sessions',
+});
+
+sessionStore.on('error', console.error.bind(console, 'connection error:'));
+
+const session = () => expressSession({
+  cookie: { secure: true },
+  name: 'jallum.sesh',
+  resave: false,
+  saveUninitialized: false,
+  secret: CONFIG.EXPRESS_SESH_SECRET,
+  store: sessionStore,
+});
+
+export { session };
