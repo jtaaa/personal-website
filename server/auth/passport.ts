2933a01240689e50dec5fe694137bd20db107ce8
jtaaa <joshuatallum@gmail.com>
2019-02-10 19:10:31 -0500

    Rename auth.ts

---
+++ server/auth/passport.ts
@@ -0,0 +1,17 @@
+import * as passport from 'passport';
+import { OAuth2Strategy as GoogleStategy } from 'passport-google-oauth';
+
+import CONFIG from '../config';
+
+passport.use(new GoogleStategy({
+    clientID: CONFIG.GOOGLE_OAUTH_CLIENT_ID,
+    clientSecret: CONFIG.GOOGLE_OAUTH_CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback',
+  },
+  (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+    return done(null, profile);
+  }
+));
+
+export { passport };
