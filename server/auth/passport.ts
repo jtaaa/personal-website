e43d63955435172b110b838639b4845ae9d60d3b
jtaaa <joshuatallum@gmail.com>
2019-02-10 19:58:08 -0500

    Change code style

--- server/auth/passport.ts
+++ server/auth/passport.ts
@@ -3,7 +3,8 @@ import { OAuth2Strategy as GoogleStategy } from 'passport-google-oauth';
 
 import CONFIG from '../config';
 
-passport.use(new GoogleStategy({
+passport.use(new GoogleStategy(
+  {
     clientID: CONFIG.GOOGLE_OAUTH_CLIENT_ID,
     clientSecret: CONFIG.GOOGLE_OAUTH_CLIENT_SECRET,
     callbackURL: '/api/auth/google/callback',
