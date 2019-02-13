e473ca28138097886f18a6453d390476b8db8f64
jtaaa <joshuatallum@gmail.com>
2019-02-12 22:35:59 -0500

    Add section and subsection model

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,4 +1,6 @@
 export * from './models/splash.model';
 export * from './models/info.model';
 export * from './models/project.model';
+export * from './models/section.model';
+export * from './models/subsection.model';
 export * from './models/user.model';
