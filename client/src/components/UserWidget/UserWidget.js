ca765af2e820c7628570d44d0a224e77bf1ebea8
jtaaa <joshuatallum@gmail.com>
2019-02-12 20:02:07 -0500

    Make UserWidget prompts dynamic as props

--- client/src/components/UserWidget/UserWidget.js
+++ client/src/components/UserWidget/UserWidget.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './UserWidget.css';
 
-const UserWidget = ({ photos }) => (
+const UserWidget = ({ photos = [], prompts = [] }) => (
   <div className="UserWidget">
     <div className="UserWidget-prompt-wrapper">
-      <Link to="/creator" className="UserWidget-prompt">Creator</Link>
+      { prompts.map(prompt => (
+      <Link to={prompt.href} className="UserWidget-prompt">{ prompt.title }</Link> ))}
     </div>
     <div className="UserWidget-bubble">
       <img className="UserWidget-photo" src={photos[0]} alt="User profile"/>
