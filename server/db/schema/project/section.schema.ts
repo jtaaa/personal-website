34232cef30645246c65fd273d9c3b6ad650bbc0f
jtaaa <joshuatallum@gmail.com>
2019-02-12 22:35:00 -0500

    Move sections to their own pages

---
+++ server/db/schema/project/section.schema.ts
@@ -0,0 +1,31 @@
+import { Schema } from 'mongoose';
+
+import { SubsectionModel } from './../../models/subsection.model';
+
+const SectionSchema = new Schema({
+  name: {
+    type: String,
+    index: true,
+    unique: true,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subsections: {
+    type: [{
+      type: Schema.Types.ObjectId,
+      ref: 'section',
+      validate: {
+        validator: async _id => SubsectionModel.count({ _id }, count => count > 0),
+        message: 'That\'s not a valid project id my dude',
+        type: 'ForeignKeyError',
+      },
+    }],
+    default: [],
+    required: true,
+  },
+});
+
+export default SectionSchema;
