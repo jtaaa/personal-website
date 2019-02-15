1216b074912f0fce9b035673c751bf809c174f47
jtaaa <joshuatallum@gmail.com>
2019-02-14 23:15:43 -0500

    Update components to reflect database changes

--- client/src/components/Section/SubSection/SubSection.js
+++ client/src/components/Section/SubSection/SubSection.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SubSection.css';
 
-const SubSection = ({ title, type, contents, backgroundColor, color }) => {
-  function renderContent() {
-    if (contents) {
-      switch (type) {
-        case 'paragraph':
-          return contents.map((content, index) => (
-            <p key={index} className="SubSection-paragraph">{ content }</p>
-          ));
-        case 'breakdown':
-        case 'rundown':
-        case 'summary':
-        case 'breakdown of de rundown':
-        case 'summary of de breakdown of the rundown':
-            return (
-              <div className="SubSection-breakdown" style={{ backgroundColor, color }}>
-                { contents.map((content, index) => (
-                <div key={index} className="SubSection-breakdown-point">{ content }</div> ))}
-              </div>
-            );
-        default:
-          return null;
-      }
-    }
-    return null;
-  }
+import ExpandList from './ExpandList/ExpandList';
 
-  return (
-    <div className="SubSection">
-      { title &&
-      <div className="SubSection-title">{ title }</div> }
-      { renderContent() }
-    </div>
-  );
+class SubSection extends Component {
+  renderContent() {
+    const { type, contents, backgroundColor, color } = this.props;
+    switch (type) {
+      case 'paragraph':
+        return contents.map((content, index) => (
+          <p key={index} className="SubSection-paragraph">{ content }</p>
+        ));
+      case 'breakdown':
+      case 'rundown':
+      case 'summary':
+      case 'breakdown of de rundown':
+      case 'summary of de breakdown of the rundown':
+        return (
+          <div className="SubSection-breakdown" style={{ backgroundColor, color }}>
+            { contents.map((content, index) => (
+            <div key={index} className="SubSection-breakdown-point">{ content }</div> ))}
+          </div>
+        );
+      case 'expand-list':
+        return (<ExpandList {...this.props} />);
+      default:
+        return null;
+    }
+  }
+  render() {
+    return (
+      <div className="SubSection">
+        { this.props.title &&
+        <div className="SubSection-title">{ this.props.title }</div> }
+        { this.renderContent() }
+      </div>
+    );
+  }
 };
 
 export default SubSection;
