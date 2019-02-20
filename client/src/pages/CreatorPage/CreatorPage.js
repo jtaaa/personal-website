0029551272907bba599e96655bf9c9b747a87dc2
jtaaa <joshuatallum@gmail.com>
2019-02-19 22:44:08 -0500

    Get and display creator project

--- client/src/pages/CreatorPage/CreatorPage.js
+++ client/src/pages/CreatorPage/CreatorPage.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './CreatorPage.css';
-import UserWidget from '../../components/UserWidget/UserWidget';
+
+import UserWidget from './../../components/UserWidget/UserWidget';
+import ProjectInfo from './../../components/ProjectInfo/ProjectInfo';
 
 class CreatorPage extends Component {
   constructor(props) {
     super (props);
 
     this.state = {
-      user: {},
+      project: {},
     };
   }
 
@@ -26,8 +28,24 @@ class CreatorPage extends Component {
       );
   }
 
+  async getCreatorProject() {
+    fetch('/api/project/creator?populate=all')
+      .then(res => {
+        if (!res.ok) {
+          throw Error('It seems I couldn\'t find the creator project.');
+        }
+        return res.json();
+      })
+      .then(project => this.setState({ project }))
+      .catch(err => process.env.REACT_APP_ENV === 'development' || err.message === undefined ?
+          console.error(err)
+        : console.log(err.message)
+      );
+  }
+
   componentDidMount() {
     this.getUser();
+    this.getCreatorProject();
   }
   
   render() {
@@ -44,6 +62,7 @@ class CreatorPage extends Component {
         <div className="CreatorPage-content-wrapper">
           <div className="Page-divider"></div>
           <div className="CreatorPage-content">
+            <ProjectInfo {...this.state.project} />
           </div>
         </div>
       </div>
