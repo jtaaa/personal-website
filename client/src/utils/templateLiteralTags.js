22ca7f0ad5b13e73b8f398940b771ec2f262b67c
jtaaa <joshuatallum@gmail.com>
2019-03-05 23:22:24 -0500

    Add time and date tagged template literals

--- client/src/utils/templateLiteralTags.js
+++ client/src/utils/templateLiteralTags.js
@@ -2,6 +2,16 @@ const ClassSet = (strs, ...states) => states[0].all === true ?
     strs.map(str => `${str.trime()} ${states.map(state => `${strs[0].trim()}-${state}`)}`)
   : `${strs[0].trim()} ${states.map(state => `${strs[0].trim()}-${state}`)}`;
 
+const date = (strs, date) => (new Date(date)).toLocaleDateString();
+
+const time = (strs, time) => {
+  const [ hms, suffix ] = (new Date(time)).toLocaleTimeString().split(' ');
+  const [ hours, minutes, seconds ] = hms.split(':');
+  return `${hours}:${minutes}${suffix.toLowerCase()}`;
+}
+
 export {
   ClassSet,
+  time,
+  date,
 };
