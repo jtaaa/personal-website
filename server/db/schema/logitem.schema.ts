75ab6d475d648483b9c230f8c3c5f8eef9e3809c
jtaaa <joshuatallum@gmail.com>
2019-03-05 21:30:16 -0500

    Add tags to logitem schema. Hooray!

--- server/db/schema/logitem.schema.ts
+++ server/db/schema/logitem.schema.ts
@@ -10,6 +10,30 @@ const logitemSchema = new Schema({
     default: Date.now,
     required: true,
   },
+  tags: {
+    type: [{
+      tag: {
+        type: String,
+        required: true,
+      },
+      references: {
+        type: [{
+          ref: {
+            type: Schema.Types.ObjectId,
+            required: true,
+          },
+          type: {
+            type: String,
+            required: true,
+          },
+        }],
+        default: [],
+        required: true,
+      },
+    }],
+    default: [],
+    required: true,
+  }
 });
 
 export { logitemSchema };
