1a654b09d3dba9e1ee48673e07707ad14f29a135
jtaaa <joshuatallum@gmail.com>
2019-03-05 21:30:26 -0500

    Add create log item action

--- client/src/redux/actions/logitem.actions.js
+++ client/src/redux/actions/logitem.actions.js
@@ -4,3 +4,17 @@ export const addLogitem = logitem => ({
   type: actionTypes.ADD_LOGITEM,
   logitem,
 });
+
+export const createLogItem = input => dispatch => fetch('/api/log', {
+  method: 'PUT',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ input }),
+}).then(res => {
+  if (!res.ok) {
+    throw new Error(`I couldn't create a logitem with input value "${input}".`)
+  }
+  return res.json().then(logitem => {
+    dispatch(addLogitem(logitem));
+    return logitem;
+  });
+});
