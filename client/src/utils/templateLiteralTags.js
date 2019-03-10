914c05f2b0de8601125a330224d3792f3956a2e7
jtaaa <joshuatallum@gmail.com>
2019-03-10 12:27:42 -0400

    Correct time tagged template literal

--- client/src/utils/templateLiteralTags.js
+++ client/src/utils/templateLiteralTags.js
@@ -4,10 +4,11 @@ const ClassSet = (strs, ...states) => states[0].all === true ?
 
 const date = (strs, date) => (new Date(date)).toDateString();
 
-const nHour = hours => hours % 12;
+const nHour = hours => hours % 12 === 0 ? 12 : hours % 12;
 const suffix = hours => hours < 12 ? 'am' : 'pm';
 const time = (strs, time) => {
-  const [ [ hours, mins ] ] = (new Date(time)).toTimeString().split(' ', 1);
+  const [ hms ] = (new Date(time)).toTimeString().split(' ', 1);
+  const [ hours, mins ] = hms.split(':', 2);
   return `${nHour(hours)}:${mins.toString().padStart(2, '0')}${suffix(hours)}`;
 }
 
