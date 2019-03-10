f51aae3f2f8b0211221f901f8a655c7086f99fa3
jtaaa <joshuatallum@gmail.com>
2019-03-10 05:01:59 -0400

    Fix time and date template literal tag for some devices

--- client/src/utils/templateLiteralTags.js
+++ client/src/utils/templateLiteralTags.js
@@ -2,12 +2,13 @@ const ClassSet = (strs, ...states) => states[0].all === true ?
     strs.map(str => `${str.trime()} ${states.map(state => `${strs[0].trim()}-${state}`)}`)
   : `${strs[0].trim()} ${states.map(state => `${strs[0].trim()}-${state}`)}`;
 
-const date = (strs, date) => (new Date(date)).toLocaleDateString();
+const date = (strs, date) => (new Date(date)).toDateString();
 
+const nHour = hours => hours % 12;
+const suffix = hours => hours < 12 ? 'am' : 'pm';
 const time = (strs, time) => {
-  const [ hms, suffix ] = (new Date(time)).toLocaleTimeString().split(' ');
-  const [ hours, minutes ] = hms.split(':');
-  return `${hours}:${minutes}${suffix.toLowerCase()}`;
+  const [ [ hours, mins ] ] = (new Date(time)).toTimeString().split(' ', 1);
+  return `${nHour(hours)}:${mins.toString().padStart(2, '0')}${suffix(hours)}`;
 }
 
 export {
