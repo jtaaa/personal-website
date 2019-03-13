cfed4432310193a29c6fb0faf030daf3e0c8b91d
jtaaa <joshuatallum@gmail.com>
2019-03-12 23:40:25 -0400

    Add colors tags when parsing

--- server/utils/Parser.ts
+++ server/utils/Parser.ts
@@ -2,6 +2,42 @@
  * Parser class for parsing my custom format
  */
 class Parser {
+  static tagColors = [
+    '#ffe5e5',
+    '#ffece5',
+    '#fff2e5',
+    '#fff9e5',
+    '#ffffe5',
+    '#f9ffe5',
+    '#f2ffe5',
+    '#ecffe5',
+    '#e6ffe6',
+    '#e5ffec',
+    '#e5fff2',
+    '#e5fff9',
+    '#e5ffff',
+    '#e5f9ff',
+    '#e5f2ff',
+    '#e5ecff',
+    '#e5e5ff',
+    '#ece5ff',
+    '#f2e5ff',
+    '#f9e5ff',
+    '#ffe5ff',
+    '#ffe5f9',
+    '#ffe5f2',
+    '#ffe5ec',
+    '#ffe5e5',
+  ];
+
+  /**
+   * Returns a random tagColor from the default list of tag colors
+   */
+  static getRandomTagColor() {
+    const randIndex = Math.floor(Math.random() * this.tagColors.length);
+    return this.tagColors[randIndex];
+  }
+
   /**
    * Parse and return tags in input with format #<tag>[@type.id, ...]
    * @param input input to be parsed
@@ -15,7 +51,7 @@ class Parser {
             return ({ type: type.toLowerCase(), ref });
             })
         : [];
-      return ({ tag: tag.replace('_', ' '), references });
+      return ({ tag: tag.replace(/\.|@|\?|!|/g, ''), references, color: this.getRandomTagColor() });
     });
   }
 };
