394c43b857429f2db1a9655c4253fe13e6a1514f
jtaaa <joshuatallum@gmail.com>
2019-05-15 00:26:35 -0400

    Updates

---
+++ precache-manifest.c10d993d55287dbafe0e8ef95145a846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc24563927ef45993c73",
+    "url": "/static/js/main.bc245639.chunk.js"
+  },
+  {
+    "revision": "23f57f9ce62e7e484028",
+    "url": "/static/js/1.23f57f9c.chunk.js"
+  },
+  {
+    "revision": "bc24563927ef45993c73",
+    "url": "/static/css/main.36c4bf67.chunk.css"
+  },
+  {
+    "revision": "7d15b9f6e015f93f9100be5c32904364",
+    "url": "/index.html"
+  }
+];
