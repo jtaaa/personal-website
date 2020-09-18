fefa96e5cb57469c98d8518bf71b562bea745b2a
Joshua Allum <joshuatallum@gmail.com>
2020-09-18 00:35:10 -0400

    Updates

---
+++ precache-manifest.0b8da0bfa3f4d308b3564ec72f68790c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5686b6aa777e7850f9a76e0c35bd2718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d67fc45b2d07fe5134",
+    "url": "/static/css/main.6520b025.chunk.css"
+  },
+  {
+    "revision": "411cb5d028e39d892ea5",
+    "url": "/static/js/2.4a55c1dd.chunk.js"
+  },
+  {
+    "revision": "df8984aab5b64b4e1669b4b331e30c91",
+    "url": "/static/js/2.4a55c1dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99d67fc45b2d07fe5134",
+    "url": "/static/js/main.10462d92.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
